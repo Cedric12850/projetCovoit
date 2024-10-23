0beea555cd076eb8a10f012fa8fd3abe28940d23
Cedric12850 <cedric.deswarte81@gmail.com>
2024-10-23 14:11:23 +0200

    ajout du dashboard easyadmin

---
+++ src/Controller/Admin/SpecificityCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Specificity;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class SpecificityCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Specificity::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
