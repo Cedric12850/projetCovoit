b0a685cf1b6226de52c23ac746042ca6def62d08
Cedric12850 <cedric.deswarte81@gmail.com>
2024-11-04 14:52:42 +0100

    récupération de la branch nico le 04/11 mais css non focntionnel et autocomplete non plus. commit avant de checkout un commit antérieur

--- migrations/Version20241104133914.php
+++ migrations/Version20241104133914.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241104080947 extends AbstractMigration
+final class Version20241104133914 extends AbstractMigration
 {
     public function getDescription(): string
     {
